5235440e62c6cd9281752324786cc6e1a96d2e75
ifUsernameIsTooLongNobodyWantToSeeItAll <149868847+ifUsernameIsTooLongNobodyWantToSeeItAll@users.noreply.github.com>
2024-01-18 23:16:41 +0800

    Update 87(0).c

--- 87(0).c
+++ 87(0).c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
-{
-    //system("shutdown -s -t 0");
-    system("shutdown -a");
-
+int main(void) {
+    system("shutdown -a"); 
     return 0;
 }
